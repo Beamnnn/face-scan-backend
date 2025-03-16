8b5b962a401f9e424e0b4f3ff9b647dbeb3206b3
Beamnnn <115034892+Beamnnn@users.noreply.github.com>
2025-03-16 23:09:28 +0700

    .

--- main.py
+++ main.py
@@ -101,4 +101,5 @@ def scan_history():
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
